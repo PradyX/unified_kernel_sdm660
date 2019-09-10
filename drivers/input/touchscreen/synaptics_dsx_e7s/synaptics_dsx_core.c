d0ef5442134c4dd2e63fdf85b99431e25f505c26
PrimoDev23 <lexx.ps2711@gmail.com>
2019-09-10 10:51:07 +0200

    touchscreen: synaptics_dsx_e7s: Use FIFO kthread for work
    * Also replace workqueue by kthread work
    
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- drivers/input/touchscreen/synaptics_dsx_e7s/synaptics_dsx_core.c
+++ drivers/input/touchscreen/synaptics_dsx_e7s/synaptics_dsx_core.c
@@ -43,6 +43,7 @@
 #include <linux/input/synaptics_dsx.h>
 #include "synaptics_dsx_core.h"
 #include <linux/input/mt.h>
+#endif
 #include <linux/kthread.h>
 #include <linux/sched/rt.h>
 
@@ -4203,7 +4204,7 @@ static int synaptics_rmi4_probe(struct platform_device *pdev)
 	const struct synaptics_dsx_hw_interface *hw_if;
 	const struct synaptics_dsx_board_data *bdata;
 
-	struct sched_param param = { .sched_priority = MAX_RT_PRIO / 2 };
+	struct sched_param param = { .sched_priority = MAX_RT_PRIO - 1 };
 
 	hw_if = pdev->dev.platform_data;
 	if (!hw_if) {
