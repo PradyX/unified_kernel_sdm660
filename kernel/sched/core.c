797d8e03fe39887490d61fc27b1c97079f063fd8
PradyX <pradyumnbhushan@gmail.com>
2020-06-19 21:12:54 +0530

    sched/core: fix function parameters
    Signed-off-by: PradyX <pradyumnbhushan@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2504,7 +2504,7 @@ void wake_up_new_task(struct task_struct *p)
 	 * Use __set_task_cpu() to avoid calling sched_class::migrate_task_rq,
 	 * as we're not fully set-up yet.
 	 */
-	__set_task_cpu(p, select_task_rq(p, task_cpu(p), SD_BALANCE_FORK, 0));
+	__set_task_cpu(p, select_task_rq(p, task_cpu(p), SD_BALANCE_FORK, 0, 1));
 #endif
 	rq = __task_rq_lock(p);
 	update_rq_clock(rq);
